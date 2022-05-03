efa747d1acc1ed86e16e907bf17a671acd093f97
kukechen <kukechenhuan@gmail.com>
2022-05-03 12:14:53 +0800

    ~

--- main/electron/py/mhFengyao.py
+++ main/electron/py/mhFengyao.py
@@ -27,14 +27,14 @@ def F_碗子山守护者(deviceId):
                 pyautogui.press('f2')
                 time.sleep(2)
                 for x in range(4):
-                    ret = baiduApi.F_大漠红色文字位置识别([window.windowArea[0], window.windowArea[1],
-                                                window.windowArea[0] + 600, window.windowArea[1] + 800])
-                    print(ret)
-                    if(ret != None):
-                        window.pointMove(ret[0], ret[1])
-                        pyautogui.click()
-                        window.F_移动到游戏区域坐标(331, 549)
-                        pyautogui.click()
+                    # ret = baiduApi.F_大漠红色文字位置识别([window.windowArea[0], window.windowArea[1],
+                    #                             window.windowArea[0] + 600, window.windowArea[1] + 800])
+                    # print(ret)
+                    # if(ret != None):
+                    window.pointMove(189, 394)
+                    pyautogui.click()
+                    window.F_移动到游戏区域坐标(331, 549)
+                    pyautogui.click()
                     pyautogui.keyDown('ctrl')
                     pyautogui.press('tab')
                     pyautogui.keyUp('ctrl')
